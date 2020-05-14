66b858847b98505e9be744beb113bb758beee103
StephenPREVOST <stephen.prevost00@gmail.com>
2020-05-14 16:05:20 +0200

    Question 3

--- gui.py
+++ gui.py
@@ -1,5 +1,4 @@
-import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QAction
 
 
 class Gui(QMainWindow):
@@ -7,13 +6,45 @@ class Gui(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.initUI()
-
     def initUI(self):
         self.statusBar().showMessage('Ready')
 
+        menubar = self.menuBar()
+        fichierMenu = menubar.add("Fichier")
+
+        openAct = QAction("ouvrir",self)
+        openAct.triggered.connect(self.open)
+        openAct.detShortcut("O")
+        openAct.setStatusTip("Ouvrir un fichier")
+
+        recAct = QAction("Enregistrer", self)
+        recAct.triggered.connect(self.rec)
+        recAct.detShortcut("S")
+        recAct.setStatusTip("Sauvegarder")
+
+        quitAct = QAction("Quitter", self)
+        quitAct.triggered.connect(self.quit)
+        quitAct.detShortcut("C")
+        quitAct.setStatusTip("Fermer la fenêtre")
+
+        fichierMenu.addAction(openAct)
+        fichierMenu.addAction(recAct)
+        fichierMenu.addAction(quitAct)
+
         self.setGeometry(300, 300, 1280, 720)
+
         self.setWindowTitle('RAVI_2')
         self.show()
+
+    def open(self):
+        print("open")
+
+    def rec(self):
+        print("rec")
+
+    def quit(self):
+        print("quit")
+        self.quit
+
 
 
